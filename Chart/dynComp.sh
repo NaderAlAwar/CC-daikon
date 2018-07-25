cb1b0123e03518db9fdea8c792132ae2f970bf87
NaderAlAwar <naderalawar@gmail.com>
2018-07-25 20:14:57 +0300

    changed /home/nader to ~

--- Chart/dynComp.sh
+++ Chart/dynComp.sh
@@ -8,7 +8,7 @@ OUTPUT_FILE="$2.decls-DynComp"
 VERSION_NAME="$3"
 OUTPUT_DIR="$4"
 JAVA7="/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java "
-CLASSPATH="-verbose:class -classpath /home/nader/Projects/defects4j/framework/lib/formatter.jar:/home/nader/Projects/defects4j/framework/projects/lib/junit-4.11.jar:/home/nader/Projects/499/Chart/v$VERSION_NAME/lib/servlet.jar:/home/nader/Projects/499/Chart/v$VERSION_NAME/build:/home/nader/Projects/499/Chart/v$VERSION_NAME/build-tests:/home/nader/Projects/defects4j/framework/lib/test_generation/runtime/evosuite-rt.jar:/home/nader/Projects/Daikon/daikon-5.6.2/daikon.jar:/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/lib/rt.jar:/usr/lib/jvm/java-1.8.0-openjdk-amd64/lib/tools.jar "
+CLASSPATH="-verbose:class -classpath ~/Projects/defects4j/framework/lib/formatter.jar:~/Projects/defects4j/framework/projects/lib/junit-4.11.jar:~/Projects/499/Chart/v$VERSION_NAME/lib/servlet.jar:~/Projects/499/Chart/v$VERSION_NAME/build:~/Projects/499/Chart/v$VERSION_NAME/build-tests:~/Projects/defects4j/framework/lib/test_generation/runtime/evosuite-rt.jar:~/Projects/Daikon/daikon-5.6.2/daikon.jar:/usr/lib/jvm/java-1.8.0-openjdk-amd64/jre/lib/rt.jar:/usr/lib/jvm/java-1.8.0-openjdk-amd64/lib/tools.jar "
 DAIKON_COMMAND="daikon.DynComp --decl-file=$OUTPUT_FILE --output-dir=$OUTPUT_DIR "
 TEST_RUNNER="edu.washington.cs.mut.testrunner.SingleTestRunner "
 
